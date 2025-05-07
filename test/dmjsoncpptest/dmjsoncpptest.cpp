0c6198ce47ad0cd6141c44115f20166d74e420dc
brinkqiang <brink.qiang@gmail.com>
2025-05-07 13:04:22 +0800

    add

--- test/dmjsoncpptest/dmjsoncpptest.cpp
+++ test/dmjsoncpptest/dmjsoncpptest.cpp
@@ -1,13 +1,25 @@
 
-#include "dmjsoncpp.h"
+#include "json/json.h"
+#include "dmformat.h"
 
 int main( int argc, char* argv[] ) {
 
-    Idmjsoncpp* module = dmjsoncppGetModule();
-    if (module)
-    {
-        module->Test();
-        module->Release();
+    Json::Value value;
+    value["sourceId"] = 18423830;
+    value["username"] = "zhang";
+    value["usercode"] = "aabbcc";
+    
+    std::string strData = value.toStyledString();
+
+    Json::Value value2;
+    Json::Reader reader;
+    bool parsingSuccess = reader.parse(strData, value2);
+    if (!parsingSuccess) {
+        fmt::print("解析失败: {}\n", reader.getFormattedErrorMessages());
+        return 1;
     }
+
+    fmt::print("{}", value2["sourceId"]);
+    
     return 0;
 }
